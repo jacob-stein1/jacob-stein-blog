87cd060ca952f4e9107ad2a904608c4c67574f0a
jacob-stein1 <jmstein@bu.edu>
2022-09-11 12:46:12 -0400

    type errors

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -9,7 +9,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 // export a default function for API route to work
 export default async function comments(req: any, res: any) {
-  const graphQLClient = new GraphQLClient(graphqlAPI, {
+  const graphQLClient = new GraphQLClient(graphqlAPI!, {
     headers: {
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
     },
