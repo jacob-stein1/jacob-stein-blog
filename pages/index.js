f95aeb7e8e4ba37138bfdb2c51d3e957f220992f
jacob-stein1 <jmstein@bu.edu>
2023-06-17 22:39:53 -0400

    Content push June 17th

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import { FeaturedPosts } from "../sections";
 // Content push 6: May 30th, 2023
 // Content push 7: June 1st, 2023
 // Content push 8: June 13th, 2023
+// Content push 9: June 17th, 2023
 
 const Home = ({ posts }) => {
   return (
